d1952659ddeea0bf3ffd3965f9268732ac65fff1
Jared Hanson <jaredhanson@gmail.com>
2012-07-01 10:39:49 -0700

    Test case where consumer callback supplies just a secret.

--- test/strategies/token-test.js
+++ test/strategies/token-test.js
@@ -64,6 +64,73 @@ vows.describe('TokenStrategy').addBatch({
       'should authenticate' : function(err, user, info) {
         assert.equal(user.username, 'bob');
       },
+      'should set info scheme to OAuth' : function(err, user, info) {
+        assert.equal(info.scheme, 'OAuth');
+      },
+      'should set consumer on info' : function(err, user, info) {
+        assert.equal(info.consumer.id, '1');
+        assert.strictEqual(info.client, info.consumer);
+      },
+    },
+  },
+  
+  // TODO: Implement test case for request with params in body
+  
+  // TODO: Implement test case for request with params in query
+  
+  'strategy handling a valid request where consumer callback supplies just a secret': {
+    topic: function() {
+      var strategy = new TokenStrategy(
+        // consumer callback
+        function(consumerKey, done) {
+          done(null, 'keep-this-secret');
+        },
+        // verify callback
+        function(accessToken, done) {
+          done(null, { username: 'bob' }, 'lips-zipped');
+        }
+      );
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user, info) {
+          self.callback(null, user, info);
+        }
+        strategy.fail = function(challenge, status) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.error = function(err) {
+          self.callback(new Error('should not be called'));
+        }
+        
+        req.url = '/1/users/show.json?screen_name=jaredhanson&user_id=1705';
+        req.method = 'GET';
+        req.headers = {};
+        req.headers['host'] = '127.0.0.1:3000';
+        req.headers['authorization'] = 'OAuth oauth_consumer_key="1234",oauth_nonce="A7E738D9A9684A60A40607017735ADAD",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1339004912",oauth_token="abc-123-xyz-789",oauth_version="1.0",oauth_signature="TBrJJJWS896yWrbklSbhEd9MGQc%3D"';
+        req.query = url.parse(req.url, true).query;
+        req.connection = { encrypted: false };
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should not generate an error' : function(err, user, info) {
+        assert.isNull(err);
+      },
+      'should authenticate' : function(err, user, info) {
+        assert.equal(user.username, 'bob');
+      },
+      'should set info scheme to OAuth' : function(err, user, info) {
+        assert.equal(info.scheme, 'OAuth');
+      },
+      'should not set consumer on info' : function(err, user, info) {
+        assert.isUndefined(info.consumer);
+      },
     },
   },
   
