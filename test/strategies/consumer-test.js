021d63d012cf9edbd608234fdc37a686b15e9227
Jared Hanson <jaredhanson@gmail.com>
2012-07-01 18:48:47 -0700

    Test case for request with missing parameters.

--- test/strategies/consumer-test.js
+++ test/strategies/consumer-test.js
@@ -1186,6 +1186,59 @@ vows.describe('ConsumerStrategy').addBatch({
     },
   },
   
+  'strategy handling a request with missing parameters': {
+    topic: function() {
+      var strategy = new ConsumerStrategy(
+        // consumer callback
+        function(consumerKey, done) {
+          done(null, { id: '1' }, 'ssh-secret');
+        },
+        // token callback
+        function(requestToken, done) {
+          done(new Error('token callback should not be called'));
+        }
+      );
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user, info) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.fail = function(challenge, status) {
+          self.callback(null, challenge, status);
+        }
+        strategy.error = function(err) {
+          self.callback(new Error('should not be called'));
+        }
+        
+        req.url = '/oauth/request_token';
+        req.method = 'POST';
+        req.headers = {};
+        req.headers['host'] = '127.0.0.1:3000';
+        req.headers['authorization'] = 'OAuth oauth_callback="http%3A%2F%2Fmacbook-air.local.jaredhanson.net%3A3001%2Foauth%2Fcallback",oauth_nonce="fNyKdt8ZTgTVdEABtUMFzcXRxF4a230q",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1341176111",oauth_version="1.0",oauth_signature="tgsFsPL%2BDDQmfEz6hbCywhO%2BrE4%3D"';
+        req.query = url.parse(req.url, true).query;
+        req.connection = { encrypted: false };
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should not generate an error' : function(err, challenge, status) {
+        assert.isNull(err);
+      },
+      'should respond with challenge' : function(err, challenge, status) {
+        assert.equal(challenge, 'OAuth realm="Clients", oauth_problem="parameter_absent"');
+      },
+      'should respond with 400 status' : function(err, challenge, status) {
+        assert.equal(status, 400);
+      },
+    },
+  },
+  
   'strategy constructed without a consumer callback or token callback': {
     'should throw an error': function (strategy) {
       assert.throws(function() { new ConsumerStrategy() });
