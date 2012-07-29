1c4841e879baf1da9c6d2048d5730a6d326e4834
Jared Hanson <jaredhanson@gmail.com>
2012-07-29 11:47:36 -0700

    Test cases for host option.

--- test/strategies/consumer-test.js
+++ test/strategies/consumer-test.js
@@ -77,6 +77,67 @@ vows.describe('ConsumerStrategy').addBatch({
     },
   },
   
+  'strategy handling a valid request without a request token using host option instead of host header': {
+    topic: function() {
+      var strategy = new ConsumerStrategy(
+        { host: '127.0.0.1:3000' },
+        // consumer callback
+        function(consumerKey, done) {
+          if (consumerKey == 'abc123') {
+            done(null, { id: '1' }, 'ssh-secret');
+          } else {
+            done(new Error('something is wrong'))
+          }
+        },
+        // token callback
+        function(requestToken, done) {
+          done(new Error('token callback should not be called'));
+        }
+      );
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user, info) {
+          self.callback(null, user, info);
+        }
+        strategy.fail = function(challenge, status) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.error = function(err) {
+          self.callback(new Error('should not be called'));
+        }
+        
+        req.url = '/oauth/request_token';
+        req.method = 'POST';
+        req.headers = {};
+        //req.headers['host'] = '127.0.0.1:3000';
+        req.headers['authorization'] = 'OAuth oauth_callback="http%3A%2F%2Fmacbook-air.local.jaredhanson.net%3A3001%2Foauth%2Fcallback",oauth_consumer_key="abc123",oauth_nonce="fNyKdt8ZTgTVdEABtUMFzcXRxF4a230q",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1341176111",oauth_version="1.0",oauth_signature="tgsFsPL%2BDDQmfEz6hbCywhO%2BrE4%3D"';
+        req.query = url.parse(req.url, true).query;
+        req.connection = { encrypted: false };
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should not generate an error' : function(err, user, info) {
+        assert.isNull(err);
+      },
+      'should authenticate' : function(err, user, info) {
+        assert.equal(user.id, '1');
+      },
+      'should set scheme to OAuth' : function(err, user, info) {
+        assert.equal(info.scheme, 'OAuth');
+      },
+      'should set callbackURL' : function(err, user, info) {
+        assert.equal(info.oauth.callbackURL, 'http://macbook-air.local.jaredhanson.net:3001/oauth/callback');
+      },
+    },
+  },
+  
   'strategy handling a valid request without a request token using PLAINTEXT signature': {
     topic: function() {
       var strategy = new ConsumerStrategy(
@@ -675,6 +736,72 @@ vows.describe('ConsumerStrategy').addBatch({
         req.method = 'POST';
         req.headers = {};
         req.headers['host'] = '127.0.0.1:3000';
+        req.headers['authorization'] = 'OAuth oauth_consumer_key="abc123",oauth_nonce="KyEf2M5ptWGDcz04jMScA2iJHkXHzkUW",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1341178687",oauth_token="wM9YRRm5",oauth_verifier="qriPjOnc",oauth_version="1.0",oauth_signature="ZP5%2FtXZcUiiD2HXKrevCL5FjY%2FM%3D"';
+        req.query = url.parse(req.url, true).query;
+        req.connection = { encrypted: false };
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should not generate an error' : function(err, user, info) {
+        assert.isNull(err);
+      },
+      'should authenticate' : function(err, user, info) {
+        assert.equal(user.id, '1');
+      },
+      'should set scheme to OAuth' : function(err, user, info) {
+        assert.equal(info.scheme, 'OAuth');
+      },
+      'should include token and verifier' : function(err, user, info) {
+        assert.equal(info.oauth.token, 'wM9YRRm5');
+        assert.equal(info.oauth.verifier, 'qriPjOnc');
+      },
+    },
+  },
+  
+  'strategy handling a valid request with a request token using host option instead of host header': {
+    topic: function() {
+      var strategy = new ConsumerStrategy(
+        { host: '127.0.0.1:3000' },
+        // consumer callback
+        function(consumerKey, done) {
+          if (consumerKey == 'abc123') {
+            done(null, { id: '1' }, 'ssh-secret');
+          } else {
+            done(new Error('something is wrong'))
+          }
+        },
+        // token callback
+        function(requestToken, done) {
+          if (requestToken == 'wM9YRRm5') {
+            done(null, 'rxt0E5hKbslOEtzxD43hclL28XBZLJsF');
+          } else {
+            done(new Error('something is wrong'))
+          }
+        }
+      );
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user, info) {
+          self.callback(null, user, info);
+        }
+        strategy.fail = function(challenge, status) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.error = function(err) {
+          self.callback(new Error('should not be called'));
+        }
+        
+        req.url = '/oauth/access_token';
+        req.method = 'POST';
+        req.headers = {};
+        //req.headers['host'] = '127.0.0.1:3000';
         req.headers['authorization'] = 'OAuth oauth_consumer_key="abc123",oauth_nonce="KyEf2M5ptWGDcz04jMScA2iJHkXHzkUW",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1341178687",oauth_token="wM9YRRm5",oauth_verifier="qriPjOnc",oauth_version="1.0",oauth_signature="ZP5%2FtXZcUiiD2HXKrevCL5FjY%2FM%3D"';
         req.query = url.parse(req.url, true).query;
         req.connection = { encrypted: false };
--- test/strategies/token-test.js
+++ test/strategies/token-test.js
@@ -82,6 +82,72 @@ vows.describe('TokenStrategy').addBatch({
     },
   },
   
+  'strategy handling a valid request using host option instead of host header': {
+    topic: function() {
+      var strategy = new TokenStrategy(
+        { host: '127.0.0.1:3000' },
+        // consumer callback
+        function(consumerKey, done) {
+          if (consumerKey == '1234') {
+            done(null, { id: '1' }, 'keep-this-secret');
+          } else {
+            done(new Error('something is wrong'))
+          }
+        },
+        // verify callback
+        function(accessToken, done) {
+          if (accessToken == 'abc-123-xyz-789') {
+            done(null, { username: 'bob' }, 'lips-zipped');
+          } else {
+            done(new Error('something is wrong'))
+          }
+        }
+      );
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user, info) {
+          self.callback(null, user, info);
+        }
+        strategy.fail = function(challenge, status) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.error = function(err) {
+          self.callback(new Error('should not be called'));
+        }
+        
+        req.url = '/1/users/show.json?screen_name=jaredhanson&user_id=1705';
+        req.method = 'GET';
+        req.headers = {};
+        //req.headers['host'] = '127.0.0.1:3000';
+        req.headers['authorization'] = 'OAuth oauth_consumer_key="1234",oauth_nonce="A7E738D9A9684A60A40607017735ADAD",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1339004912",oauth_token="abc-123-xyz-789",oauth_version="1.0",oauth_signature="TBrJJJWS896yWrbklSbhEd9MGQc%3D"';
+        req.query = url.parse(req.url, true).query;
+        req.connection = { encrypted: false };
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should not generate an error' : function(err, user, info) {
+        assert.isNull(err);
+      },
+      'should authenticate' : function(err, user, info) {
+        assert.equal(user.username, 'bob');
+      },
+      'should set scheme to OAuth' : function(err, user, info) {
+        assert.equal(info.scheme, 'OAuth');
+      },
+      'should set consumer' : function(err, user, info) {
+        assert.equal(info.consumer.id, '1');
+        assert.strictEqual(info.client, info.consumer);
+      },
+    },
+  },
+  
   'strategy handling a valid request with credentials in header using PLAINTEXT signature': {
     topic: function() {
       var strategy = new TokenStrategy(
