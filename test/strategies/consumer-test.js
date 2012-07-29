2125bae266c022be5dc2459a7ef3711383d9a8bb
Jared Hanson <jaredhanson@gmail.com>
2012-07-29 12:12:59 -0700

    Implement ignoreVersion option.

--- test/strategies/consumer-test.js
+++ test/strategies/consumer-test.js
@@ -137,6 +137,67 @@ vows.describe('ConsumerStrategy').addBatch({
     },
   },
   
+  'strategy handling a valid request without a request token placing credentials in header using 1.0A version': {
+    topic: function() {
+      var strategy = new ConsumerStrategy(
+        { ignoreVersion: true },
+        // consumer callback
+        function(consumerKey, done) {
+          if (consumerKey == 'abc123') {
+            done(null, { id: '1' }, 'ssh-secret');
+          } else {
+            done(new Error('something is wrong'))
+          }
+        },
+        // token callback
+        function(requestToken, done) {
+          done(new Error('token callback should not be called'));
+        }
+      );
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user, info) {
+          self.callback(null, user, info);
+        }
+        strategy.fail = function(challenge, status) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.error = function(err) {
+          self.callback(new Error('should not be called'));
+        }
+        
+        req.url = '/oauth/request_token';
+        req.method = 'POST';
+        req.headers = {};
+        req.headers['host'] = '127.0.0.1:3000';
+        req.headers['authorization'] = 'OAuth oauth_callback="http%3A%2F%2Fmacbook-air.local.jaredhanson.net%3A3001%2Foauth%2Fcallback",oauth_consumer_key="abc123",oauth_nonce="fNyKdt8ZTgTVdEABtUMFzcXRxF4a230q",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1341176111",oauth_version="1.0A",oauth_signature="VfpGYYsNM4Ih0Lt7JsIbJz6%2FJM4%3D"';
+        req.query = url.parse(req.url, true).query;
+        req.connection = { encrypted: false };
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should not generate an error' : function(err, user, info) {
+        assert.isNull(err);
+      },
+      'should authenticate' : function(err, user, info) {
+        assert.equal(user.id, '1');
+      },
+      'should set scheme to OAuth' : function(err, user, info) {
+        assert.equal(info.scheme, 'OAuth');
+      },
+      'should set callbackURL' : function(err, user, info) {
+        assert.equal(info.oauth.callbackURL, 'http://macbook-air.local.jaredhanson.net:3001/oauth/callback');
+      },
+    },
+  },
+  
   'strategy handling a valid request without a request token using host option instead of host header': {
     topic: function() {
       var strategy = new ConsumerStrategy(
